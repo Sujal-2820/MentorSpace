778c59ea0cea2480558a838e785e49d3a4b5f6e3
Sujal Soni <sujal28soni@gmail.com>
2024-12-15 17:28:35 +0530

    Added dashboard layout files and folders

---
+++ src/app/dashboard/layout.jsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { useState } from 'react'
+import { SidebarProvider } from '@/components/ui/sidebar'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import Content from './components/Content'
+
+export default function DashboardLayout({
+children,
+}) {
+const [selectedScreen, setSelectedScreen] = useState('Profile')
+const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+
+return (
+  <SidebarProvider>
+    <div className="flex h-screen w-full flex-col">
+      <Navbar toggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)} />
+      <div className="relative flex flex-1 overflow-hidden">
+        <Sidebar 
+          selectedScreen={selectedScreen} 
+          setSelectedScreen={setSelectedScreen}
+          isOpen={isSidebarOpen}
+          setIsOpen={setIsSidebarOpen}
+        />
+        <Content selectedScreen={selectedScreen} />
+      </div>
+    </div>
+  </SidebarProvider>
+)
+}
